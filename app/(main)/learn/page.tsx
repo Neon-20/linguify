b545982f8e95edf89eb88ec524a86f064a2115e7
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-11 03:16:32 +0530

    feature: added root /learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,22 @@
+import FeedWrapper from "@/components/FeedWrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import { Header } from "./_components/Header";
+import { UserProgress } from "@/components/UserProgress";
+
 const LearnPage = () => {
     return ( 
-        <div>
-            Learn Page
+        <div className="flex gap-[48px] px-2">
+            <FeedWrapper>
+            <Header title="Spanish"/>
+            </FeedWrapper>
+            <StickyWrapper>
+                <UserProgress
+                activeCourse={{title:"Spanish", imageSrc:"/es.svg"}}
+                hearts={5}
+                points={10}
+                hasActiveSubscription={false}
+                />
+            </StickyWrapper>
         </div>
     );
 }
