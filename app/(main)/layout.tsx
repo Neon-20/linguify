a0bc1e853486dfcc58909f6d5431cd9de74132d1
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-11 03:15:42 +0530

    style: fixing width for auto-expandable

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -13,7 +13,7 @@ const MainLayout = ({
         <MobileHeader/>
         <SideBar className="hidden lg:flex"/>
         <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-            <div className="bg-red-500 h-full">
+            <div className="max-w-[1056px] mx-auto p-6 h-full">
             {children}
             </div>
         </main>
