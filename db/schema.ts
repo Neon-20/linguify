7082496d1cc66fb5e43b2009d6a2d6f9f0a1ff17
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-13 02:58:13 +0530

    feat: added drizzle db (query,schema,config)

---
+++ db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable,serial,text } from "drizzle-orm/pg-core";
+
+
+export const courses = pgTable("courses",{
+    id:serial("id").primaryKey(),
+    title:text("title").notNull(),
+    imageSrc:text("imageSrc").notNull(),
+})
