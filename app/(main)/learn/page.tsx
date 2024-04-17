176cb02f986a0685ee31a6035a1d3a2d7b76d113
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-18 04:23:28 +0530

    feat: added units to /learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,13 +2,21 @@ import FeedWrapper from "@/components/FeedWrapper";
 import StickyWrapper from "@/components/sticky-wrapper";
 import { Header } from "./_components/Header";
 import { UserProgress } from "@/components/UserProgress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
+import { lessons } from '../../../db/schema';
+import Unit from "./_components/Unit";
 
 const LearnPage = async() => {
-const userProgress = await getUserProgress();
+const userProgressData =  getUserProgress();
+const unitsData =  getUnits();
+
+const [userProgress, units] = await Promise.all([userProgressData, unitsData]);
 
 if(!userProgress || !userProgress.activeCourse){
+    redirect("/courses");
+}
+if(!units){
     redirect("/courses");
 }
 
@@ -16,6 +24,19 @@ if(!userProgress || !userProgress.activeCourse){
         <div className="flex gap-[48px] px-2">
             <FeedWrapper>
             <Header title = {userProgress.activeCourse.title} />
+            {units.map((unit) => (
+                <div key={unit.id} className="mb-10">
+                    <Unit
+                    id={unit.id}
+                    title={unit.title}
+                    description={unit.description}
+                    order={unit.order}
+                    lessons={unit.lessons}
+                    activeLesson={undefined}
+                    activeLessonPercentage={0}
+                    />
+                </div>
+            ))}
             </FeedWrapper>
             <StickyWrapper>
                 <UserProgress
