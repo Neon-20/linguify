925c23b5a60f83a5330fbe625d3f083b7b041287
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-17 22:34:09 +0530

    refactor: modified seed file

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -10,6 +10,13 @@ const db = drizzle(sql,{schema});
 const main = async() => {
     try{
     console.log("Seeding started");
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengesOptions);
+    await db.delete(schema.challengesProgress);
     await db.insert(schema.courses).values([
         {
             id:1,
@@ -32,6 +39,68 @@ const main = async() => {
             imageSrc:"/it.svg",
         }
     ])
+    
+    await db.insert(schema.units).values([
+        {
+            id:1,
+            courseId:1,
+            title:"Indian",
+            description:"This is indian",
+            order:1,
+        },
+    ])
+    
+    await db.insert(schema.lessons).values([
+        {
+            id:1,
+            title:"Nouns",
+            unitId:1,
+            order:1,
+        },
+        {
+            id:2,
+            title:"Verbs",
+            unitId:1,
+            order:2,
+        },
+    ]);
+
+    await db.insert(schema.challenges).values([
+        {
+            id:1,
+            lessonId:1, //Noun
+            type:"SELECT",
+            order:1,
+            question:'Which one of these is "the man"?'
+        },
+    ]);
+    await db.insert(schema.challengesOptions).values([
+        {
+            id:1,
+            challengeId:1, //Noun
+            imageSrc:"/man.svg",
+            text:"el hombre",
+            audioSrc:"es_man.mp3",
+            correct:true,
+        },
+        {
+            id:2,
+            challengeId:1, //Noun
+            imageSrc:"/woman.svg",
+            text:"la mujer",
+            audioSrc:"es_woman.mp3",
+            correct:false,
+        },
+        {
+            id:3,
+            challengeId:1, //Noun
+            imageSrc:"/robot.svg",
+            text:"el robot",
+            audioSrc:"es_robot.mp3",
+            correct:false,
+        },
+    ]);
+    
 
     console.log("Seeding finished");    
     }
