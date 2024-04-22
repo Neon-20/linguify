957928327015253a25829a78ec7fa9c7666fa07c
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-22 12:29:53 +0530

    style: added another button variant `locked`

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,13 +9,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        locked:'bg-neutral-200 border-b-4 active:border-b-0 text-primary-foreground hover:bg-neutral-200/90 border-neutral-400',
         default:
           "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
         primary:
           "bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
         primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
         secondary:
-          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0",
+          "bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-2",
         secondaryOutline: "bg-white text-green-500 hover:bg-slate-100",
         danger:
           "bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0",
