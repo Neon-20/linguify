f6c4c52182c83c85506c4e478896fb8740929ebd
Pranav Rajveer <rajveer23pr@gmail.com>
2024-04-27 02:44:32 +0530

    feat: added exitModal UI

---
+++ components/modals/useExitModal.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useExitModal } from "@/store/use-exit-modal";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
+const ExitModal = () => {
+    const router = useRouter();
+    const[isClient,setIsClient] = useState<boolean>(false)
+    const {isOpen,close,open} = useExitModal();
+
+    //Doing this to avoid hydration errors
+    useEffect(()=>{
+        setIsClient(true);
+    },[])
+    if(!isClient){
+        return null;
+    }
+
+//  isOpen and close are states from zustand
+    return ( 
+        <Dialog open={isOpen} onOpenChange={close}>
+            <DialogContent className="max-w-md mx-auto">
+            <DialogHeader>
+                <div className="items-center justify-center flex w-full mb-5">
+                <Image
+                src="mascot_sad.svg"
+                alt="Mascot"
+                height={80}
+                width={80}
+                />
+                </div>
+                <DialogTitle className="text-center
+                font-bold text-2xl">
+                Wait don&apos;t go!!!
+                </DialogTitle>
+                <DialogDescription className="text-center text-base">
+                You&apos;re about to leave the lesson. Are you sure?
+                </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+                <div className="flex flex-col gap-y-3 w-full">
+                <Button variant="primary" size="lg" className="w-full"
+                onClick={close}>
+                    Keep Learning
+                </Button>
+                <Button variant="dangerOutline" size="lg" className="w-full"
+                onClick={()=>{
+                    close();
+                    router.push("/learn")
+                }}>
+                    End Session
+                </Button>
+                </div>
+            </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default ExitModal;
