991b11112c39211c28d87d658cac09063ea7e3d3
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-01 23:53:54 +0530

    style: dark mode color changes

--- app/lesson/_components/CardLesson.tsx
+++ app/lesson/_components/CardLesson.tsx
@@ -46,9 +46,9 @@ const CardLesson = ({
         onClick={handleClick}
         className={cn
         ("h-full border-2 border-b-4 rounded-xl hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
-            selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
-            selected && status === "correct" && "border-green-300 bg-green-100 hover:bg-green-100",
-            selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100",
+            selected && "dark:bg-sky-300 dark:text-black border-sky-300 bg-sky-100 hover:bg-sky-100",
+            selected && status === "correct" && "dark:border-green-500 border-green-300 bg-green-100 dark:bg-green-200 hover:bg-green-100",
+            selected && status === "wrong" && "dark:bg-rose-200 dark:hover:bg-rose-200  border-rose-300 bg-rose-100 hover:bg-rose-100",
             disabled && "pointer-events-none opacity-50",
             type==="ASSIST" && "lg:p-3 w-full"
         )
@@ -73,7 +73,7 @@ const CardLesson = ({
             {type==="ASSIST" && <div/>}
             <p className={cn
                 ("text-neutral-600 dark:text-neutral-300 text-sm lg:text-base",
-                selected && "text-sky-500",
+                selected && "dark:text-black text-sky-500",
                 selected && status === "correct" && "text-green-500",
                 selected && status === "wrong" && "text-rose-500"
                 )
@@ -84,10 +84,10 @@ const CardLesson = ({
                 (`lg:w-[30px] flex lg:h-[30px] w-[20px] h-[20px] 
                 items-center justify-center 
                 rounded-lg border-2 lg:text-[15px] text-xs font-semibold
-                text-neutral-400 dark:text-neutral-300`,
-                selected && "border-sky-300 text-sky-500",
-                selected && status === "correct" && "border-green-500 text-green-500",
-                selected && status === "wrong" && "border-rose-500 text-rose-500"
+                text-black dark:text-neutral-300 `,
+                selected && "dark:border-sky-400 dark:text-black border-sky-300 text-sky-500",
+                selected && status === "correct" && "dark:border-green-500 border-green-500 text-green-500",
+                selected && status === "wrong" && "dark:border-rose-500 border-rose-500 text-rose-500"
             )
             }>
                 {shortcut}
