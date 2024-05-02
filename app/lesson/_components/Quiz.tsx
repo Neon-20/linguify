402bc1c8adc72550e75673b834a668f005f77845
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-02 13:01:33 +0530

    feat: added stuff to quiz and result component

--- app/lesson/_components/Quiz.tsx
+++ app/lesson/_components/Quiz.tsx
@@ -2,11 +2,19 @@
 
 import { challenges } from "@/db/schema"
 import { challengesOptions } from '../../../db/schema';
-import { useState } from "react";
+import { useState,useTransition } from "react";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 import Header from "./Header";
 import QuestionBubble from "./QuestionBubble";
 import Challenge from "./ChallengeOpt";
 import Footer from "./Footer";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
+import { toast } from "sonner";
+import { reduceHearts } from "@/actions/user-progress";
+import { useAudio } from "react-use";
+import Image from "next/image";
+import ResultCard from "./result-card";
 
 
 interface QuizProps{
@@ -30,8 +38,13 @@ const Quiz = ({
     const [hearts,setHearts] = useState(initialHearts);
     const [percentage,setPercentage] = useState(initialPercentage);
     const [challenges] = useState(initialLessonChallenges);
-    const[status,setStatus] = useState<"correct" | "wrong" | "none">("none")
-    const[selectedOption,setSelectedOption] = useState<number>()
+    const [status,setStatus] = useState<"correct" | "wrong" | "none">("none")
+    const [selectedOption,setSelectedOption] = useState<number>()
+    const [pending,startTransition] = useTransition();
+    const [correctAudio,_,correctControl] = useAudio({src:"/correct.wav"})
+    const [incorrectAudio,_i,incorrectControl] = useAudio({src:"/incorrect.wav"})
+    const [finishAudio,_f,finishControl] = useAudio({src:"/finish.wav"})
+
 
     //find current challenge
     const[activeIndex,setActiveIndex] = useState(()=>{
@@ -42,17 +55,140 @@ const Quiz = ({
     const currentChallenge = challenges[activeIndex];//current active challenge
     const options = currentChallenge?.challengesOptions ?? [];
 
+
+    const onNext = () => {
+        setActiveIndex((current)=>current+1);
+    }
+    const { width, height } = useWindowSize();
+    if(true || !currentChallenge){
+        finishControl.play();
+        return(
+        <>
+        {finishAudio}
+        <Confetti
+        width={width}
+        height={height}
+        recycle={false}
+        numberOfPieces={1000}
+        />
+        <div className="flex flex-col gap-y-4 lg:gap-y-8
+        items-center justify-center h-full max-w-lg mx-auto">
+            <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="hidden lg:block"
+            height={100}
+            width={100}
+            />
+            <Image
+            src="/finish.svg"
+            alt="Finish"
+            className="block lg:hidden"
+            height={50}
+            width={50}
+            />
+            <h1 className="text-xl text-center lg:text-3xl
+            font-bold text-neutral-700">
+                Great Job! <br/>
+                You&apos;ve completed the lesson.
+            </h1>
+            <div className="flex items-center gap-x-4 w-full">
+            <ResultCard
+            variant="points"
+            value={challenges.length * 10}    
+            />
+            <ResultCard
+            variant="hearts"
+            value={hearts}    
+            />
+            </div>
+            <Footer
+            disabled={pending}
+            status={status}
+            onCheck={()=> {}}
+            />
+        </div>
+        </>
+        )
+}
+
     const title = currentChallenge.type === 'ASSIST'
     ? "Select the correct meaning"
     : currentChallenge.question;
+
 
     const onSelect = (id:number)=>{
         if(status !== "none") return
         setSelectedOption(id);
     }
+  
+
+    // This function ensures that always onContinue is fired no matter answer wrong or correct.
+    const onContinue = () => {
+    if(!selectedOption) return;
+    if(status === "wrong"){
+        setStatus("none");
+        setSelectedOption(undefined)
+        return;
+    }
+    if(status === "correct"){
+        onNext();
+        setStatus("none");
+        setSelectedOption(undefined)
+        return;
+    }
+    
+    //The time where we neither have correct or wrong status
+
+    const correctOption = options.find((option)=>option.correct);
+    if(!correctOption) return;
+
+    if(correctOption && correctOption.id === selectedOption) {
+        startTransition(()=>{
+            upsertChallengeProgress(currentChallenge.id)
+            .then((res)=>{
+            if(res?.error){
+            toast.error("No hearts found 💔")
+            return;
+            }
+            correctControl.play();
+            setStatus("correct")
+            setPercentage((prev)=>prev+100/challenges.length)
+
+            if(initialPercentage === 100){
+                setHearts((prev)=>Math.min(prev+1,5))
+            }
+            })  
+            .catch(()=>toast.error("Something went wrong"))
+        });
+    }   
+
+    // if answer is wrong, reduce hearts
+    else{
+    startTransition(()=>{
+        reduceHearts(currentChallenge.id)
+        .then((res)=> {
+            if(res?.error === "hearts"){
+                console.error("Missing hearts");
+                return;
+            }
+            incorrectControl.play()
+            setStatus("wrong");
+            
+            if(!res?.error){
+                setHearts((prev)=>Math.max(prev-1,0))
+            }
+        })
+        .catch(()=> toast.error("Something went wrong"));
+    })
+    }
+        
+}
 
     return ( 
         <>
+        {incorrectAudio}
+        {correctAudio}
             <Header
             hearts={hearts}
             percentage={percentage}
@@ -73,7 +209,7 @@ const Quiz = ({
                     <Challenge
                     options = {options}
                     onSelect={onSelect}
-                    disabled={false}
+                    disabled={pending}
                     status={status}
                     selectedOption={selectedOption}
                     type={currentChallenge.type}
@@ -83,9 +219,9 @@ const Quiz = ({
                 </div>
             </div>
             <Footer
-            disabled={!selectedOption}
+            disabled={pending || !selectedOption}
             status={status}
-            onCheck={()=>{}}
+            onCheck={onContinue}
             />
         </>
     );
---
+++ app/lesson/_components/result-card.tsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+interface ResultCardProps{
+    variant:"points" | "hearts"
+    value:number
+}
+
+const ResultCard = ({
+    variant,
+    value
+}:ResultCardProps) => {
+    const imageSrc = variant === "hearts" ? "/heart.svg" : "/points.svg";
+    return ( 
+        <div className={cn("rounded-2xl border-2 w-full",
+            variant === "points" && "bg-orange-400 border-orange-400",
+            variant === "hearts" && "bg-rose-400 border-rose-400"
+        )}>
+            <div className={cn(
+            `p-1.5 text-white rounded-t-xl font-bold text-center uppercase text-xs`,
+            variant === "points" && "bg-orange-400",
+            variant === "hearts" && "bg-rose-400"
+            )}>
+                {variant === "hearts" ? "Hearts Left" : "Total XP"}
+            </div>
+            <div className={cn
+                (`rounded-2xl bg-white items-center 
+                flex justify-center font-bold p-6 text-lg`,
+                variant === "points" && "text-orange-400",
+                variant === "hearts" && "text-rose-400"
+            )}>
+                <Image
+                src={imageSrc}
+                alt="Icon"
+                height={30}
+                width={30}
+                className="mr-2"
+                />
+                {value}
+            </div>
+        </div>
+    );
+}
+
+export default ResultCard;
