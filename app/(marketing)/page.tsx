90a453b553374a1d5ee14d3ad0676f7e4171ca75
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-04 00:40:55 +0530

    feat: added VAPI voice assistant

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,6 +5,8 @@ import { Loader } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ConnectJson from "./connect";
+import VapiAssistant from "@/components/VapiAssistant";
+
 
 export default function Home() {
   return (
@@ -20,6 +22,9 @@ export default function Home() {
     />
     </div>
     <div className="flex flex-col items-center gap-y-6">
+        <div className="items-center justify-center flex">
+        <VapiAssistant/>
+        </div>
     <h1 className="font-bold text-xl lg:text-3xl
     text-neutral-600 max-w-[600px] text-center
     dark:bg-clip-text dark:text-transparent dark:bg-gradient-to-tr from-slate-500 to-neutral-300/90
---
+++ components/VapiAssistant.tsx
@@ -0,0 +1,44 @@
+"use client"
+import Vapi from "@vapi-ai/web";
+import { useState, useEffect } from "react";
+import { HoverBorderGradient } from "./hover-border-gradient";
+import { Mic } from "lucide-react";
+
+
+export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_API!);
+  
+export default function VapiAssistant() {
+  const [callStatus, setCallStatus] = useState("inactive");
+  
+  const start = async () => { 
+    setCallStatus("loading");
+    const response = vapi.start(process.env.NEXT_PUBLIC_ASSISTANT_ID!);
+  };
+
+  const stop = () => {
+    setCallStatus("loading");
+    vapi.stop();
+  };
+
+  useEffect(() => {
+    vapi.on("call-start", () => setCallStatus("active"));
+    vapi.on("call-end", () => setCallStatus('inactive'));
+    
+    return () => vapi.removeAllListeners();
+  }, [])
+  
+  return (
+    <div className="flex justify-center text-center">
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2 cursor-pointer"
+      >
+        <Mic className="h-5 w-5 mr-1"/>
+      {callStatus === "inactive" ? (<span onClick={start}>Give it a try</span>) : null}
+      {callStatus === "loading" ? <span className="animate-pulse">One Sec..</span> : null}
+      {callStatus === "active" ? (<span  onClick={stop}>Press to Stop</span>) : null}
+      </HoverBorderGradient>
+    </div>
+  );
+}
