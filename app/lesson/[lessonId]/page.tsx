549a2eec1dcf5c95742f2163ffe6db18d9ef0aca
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-04 00:43:14 +0530

    feat: added `/${lessonId}` page

---
+++ app/lesson/[lessonId]/page.tsx
@@ -0,0 +1,40 @@
+import { getLesson, getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
+import Quiz from "../_components/Quiz";
+
+type Props = {
+    params:{
+        lessonId:number
+    }
+}
+
+
+const LessonIdPage = async({
+    params,
+}:Props) => {
+    const lessonData =  getLesson(params.lessonId);
+    const userProgressData =  getUserProgress();
+
+    const[lesson,userProgress] = await Promise.all([lessonData,userProgressData]);
+
+    
+    if(!lesson || !userProgress) {
+        redirect("/learn");
+    }
+
+    const initialPercentage = lesson.challenges
+    .filter((challenge)=>challenge.completed)
+    .length / lesson.challenges.length*100;
+
+    return ( 
+        <Quiz
+        initialLessonId = {lesson.id}
+        initialLessonChallenges = {lesson.challenges}
+        initialHearts = {userProgress.hearts}
+        initialPercentage = {initialPercentage}
+        userSubscription= {null}
+        />
+    );
+}
+
+export default LessonIdPage;
