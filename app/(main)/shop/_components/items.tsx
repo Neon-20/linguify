637d1aa5dfbf522a50187f693fe0672663f78c6b
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-06 02:19:16 +0530

    feat: added and shop and animated tooltip

--- app/(main)/shop/_components/items.tsx
+++ app/(main)/shop/_components/items.tsx
@@ -3,6 +3,7 @@
 import { refillHearts } from "@/actions/user-progress";
 import { createStripeUrl } from "@/actions/user-subscription";
 import { Button } from "@/components/ui/button";
+import { POINTS_TO_REFILL } from "@/constants";
 import Image from "next/image";
 import { useTransition } from "react";
 import { toast } from "sonner";
@@ -13,7 +14,6 @@ type Props = {
     hasActiveSubscription:boolean;
 }
 
-const POINTS_TO_REFILL = 20;
 
 const ItemsPage = ({
     hearts,
