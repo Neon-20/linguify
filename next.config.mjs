b96d1cfc73445e071e50f600c90d9a1fcc9bd7d4
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-06 02:14:51 +0530

    chore: package and next-config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'img.clerk.com',
+            port: '',
+            pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
