e68c3ed472ce837bf83cfe7668c1b98c48d4ac0b
Pranav Rajveer <rajveer23pr@gmail.com>
2024-05-06 19:12:59 +0530

    refac: added quests constant

--- constants.ts
+++ constants.ts
@@ -1,2 +1,21 @@
 export const POINTS_TO_REFILL = 10;
 export const DAY_IN_MS = 86_400_000;
+
+export const quests = [
+    {
+        title:"Earn 20 XP",
+        value:20
+    },
+    {
+        title:"Earn 50 XP",
+        value:50
+    },
+    {
+        title:"Earn 100 XP",
+        value:100
+    },
+    {
+        title:"Earn 1000 XP",
+        value:1000
+    }
+]
